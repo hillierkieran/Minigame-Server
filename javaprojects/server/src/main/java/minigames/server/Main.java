54f43adac23c243c8373bce9fb295c25b6ec76a6
William Billingsley <wbillingsley@cantab.net>
2022-07-28 16:14:51 +1000

    Add vertx imports to server
    This pulls in the vertx libraries and starts to import them into the code, although there's not much server there yet.

--- javaprojects/server/src/main/java/minigames/server/Main.java
+++ javaprojects/server/src/main/java/minigames/server/Main.java
@@ -1,9 +1,32 @@
 package minigames.server;
 
-public class Main {
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Launcher;
+
+/**
+ * A class for starting up the game server.
+ * 
+ * This is shaped a little by things that Vertx needs to start itself up
+ */
+public class Main extends AbstractVerticle {
 
     public static void main(String... args) {
-        System.out.println("Still being written...");
+        // Ask the Vertx launcher to launch our "Verticle".
+        // This will cause Vert.x to start itself up, and then create a Main object and call our Main::start method
+        Launcher.executeCommand("run", "minigames.server.Main");
     }
+
+    GameServer gameServer;
+
+    /**
+     * The start method is called by vertx to initialise this Verticle.
+     */
+    @Override
+    public void start(Promise<Void> promise) {
+        System.out.println("Creating game server");
+        gameServer = new GameServer(vertx);
+    }
+
 
 }
